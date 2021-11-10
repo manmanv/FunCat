70c2d76eaa6f9328c1bb27e807fcc059bae6095e
manmanv <1850071598@qq.com>
2021-11-10 17:54:39 +0800

    f

---
+++ 08-书籍购物车案例/03-main.js
@@ -0,0 +1,59 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+        heads:['','书籍名称','出版日期','价格','购买数量','操作'],
+        books:[
+            {
+                id:1,
+                name:'《算法导论》',
+                date:'2006-9',
+                price:85.00,
+                count:1
+            },
+            {
+                id:2,
+                name:'《UNIX编程艺术》',
+                date:'2006-2',
+                price:59.00,
+                count:1
+            },
+            {
+                id:3,
+                name:'《编程珠玑》',
+                date:'2008-2',
+                price:39.00,
+                count:1
+            },
+            {
+                id:4,
+                name:'《算法之路》',
+                date:'20012-2',
+                price:159.00,
+                count:1
+            }
+        ]
+    },
+    methods:{
+        getFinalPrice(price){
+            return '￥'+ price.toFixed(2)
+        },
+        increment(index){
+            this.books[index].count++
+        },
+        decrement(index){
+            this.books[index].count--
+        },
+        removeHandle(index){
+            this.books.splice(index,1)
+        }
+    },
+    computed:{
+        totalPrice(){
+            let totalPrice=0
+            for(let i=0;i<this.books.length;i++){
+                totalPrice+=this.books[i].price*this.books[i].count
+            }
+            return totalPrice
+        }
+    }
+})
