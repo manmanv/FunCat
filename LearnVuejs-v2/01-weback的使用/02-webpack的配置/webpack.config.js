8a3ecaa077a06f817a25a9daed8df43dceec13fb
manmanv <1850071598@qq.com>
2021-11-22 17:16:43 +0800

    k

--- LearnVuejs-v2/01-weback的使用/02-webpack的配置/webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-module.exports = {
-    entry:'./src/main.js',//入口
-    output:{
-        path:path.resolve(__dirname,'dist'),//动态获取路径
-        filename:'bundle.js'
-    },//出口 
-}
